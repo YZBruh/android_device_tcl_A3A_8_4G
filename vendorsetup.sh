00e6263c0945c4f5fb0ad2cc32fdca27c6dcd6d1
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-13 19:56:17 +0300

    Use AOSP sources

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_A3A_8_4G-user
-add_lunch_combo omni_A3A_8_4G-userdebug
-add_lunch_combo omni_A3A_8_4G-eng
+add_lunch_combo twrp_A3A_8_4G-user
+add_lunch_combo twrp_A3A_8_4G-userdebug
+add_lunch_combo twrp_A3A_8_4G-eng
